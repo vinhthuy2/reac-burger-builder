5b414267dc58040de1a85082a9b4d845ec54848a
Tran Vinh Thuy <thuy.tranvinh@vn.bosch.com>
2019-04-02 15:29:12 +0700

    add navigation checkout

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,49 +1,34 @@
 import React, { Component } from 'react';
 
 import classes from './Modal.css';
-import Aux from '../../../hoc/_Aux/_Aux';
+import Aux from '../../../hoc/Aux/Aux';
 import Backdrop from '../Backdrop/Backdrop';
 
 class Modal extends Component {
-<<<<<<< HEAD:src/components/UI/Modal/Modal.js
-  shouldComponentUpdate(nextProps, nextState) {
-    return (
-      nextProps.show !== this.props.show ||
-      nextProps.children !== this.props.children
-    );
-  }
-=======
-  shouldComponentUpdate = (nextProps, nextState) => {
-    return nextProps.show !== this.props.show || nextProps.children !== this.props.children;
-  };
->>>>>>> 3bcd31987bf2b61258d99a81648644414a79f4b0:src/components/UI/Modal/Modal.jsx
 
-  componentWillUpdate() {
-    console.log('[Modal] WillUpdate');
-  }
+    shouldComponentUpdate ( nextProps, nextState ) {
+        return nextProps.show !== this.props.show || nextProps.children !== this.props.children;
+    }
 
-  render() {
-    return (
-      <Aux>
-<<<<<<< HEAD:src/components/UI/Modal/Modal.js
-        <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
-=======
-        <Backdrop clicked={this.props.modalClosed} show={this.props.show}/>
->>>>>>> 3bcd31987bf2b61258d99a81648644414a79f4b0:src/components/UI/Modal/Modal.jsx
-        <div
-          className={classes.Modal}
-          style={{
-            transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
-            opacity: this.props.show ? '1' : '0',
-            display: 'flex',
-            justifyContent: 'center'
-          }}
-        >
-          {this.props.children}
-        </div>
-      </Aux>
-    );
-  }
+    componentWillUpdate () {
+        console.log('[Modal] WillUpdate');
+    }
+
+    render () {
+        return (
+            <Aux>
+                <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
+                <div
+                    className={classes.Modal}
+                    style={{
+                        transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                        opacity: this.props.show ? '1' : '0'
+                    }}>
+                    {this.props.children}
+                </div>
+            </Aux>
+        )
+    }
 }
 
 export default Modal;
