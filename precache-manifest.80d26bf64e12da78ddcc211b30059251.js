752954ddd2957fdcbf3aa3b0104d948b041626cf
Tran Vinh Thuy <thuy.tranvinh@vn.bosch.com>
2019-05-09 14:30:15 +0700

    Updates

---
+++ precache-manifest.80d26bf64e12da78ddcc211b30059251.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "7997f2f9985fcfe1a800",
+    "url": "/burger-builder-deployment/static/css/4.b4c28651.chunk.css"
+  },
+  {
+    "revision": "e858076e8f684f9028a9",
+    "url": "/burger-builder-deployment/static/css/main.e9f3a487.chunk.css"
+  },
+  {
+    "revision": "1c4af3e69d544d8fdcee",
+    "url": "/burger-builder-deployment/static/js/runtime~main.4922e6f4.js"
+  },
+  {
+    "revision": "56d3df5a9d2a05dfa430",
+    "url": "/burger-builder-deployment/static/js/2.fa016389.chunk.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger-builder-deployment/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "227071d0affd2a5c6d83",
+    "url": "/burger-builder-deployment/static/js/3.a2ffd715.chunk.js"
+  },
+  {
+    "revision": "e858076e8f684f9028a9",
+    "url": "/burger-builder-deployment/static/js/main.2e72faf4.chunk.js"
+  },
+  {
+    "revision": "7997f2f9985fcfe1a800",
+    "url": "/burger-builder-deployment/static/js/4.2779a391.chunk.js"
+  },
+  {
+    "revision": "bb78775113cfd63c712d",
+    "url": "/burger-builder-deployment/static/js/5.5c03252c.chunk.js"
+  },
+  {
+    "revision": "bb78775113cfd63c712d",
+    "url": "/burger-builder-deployment/static/css/5.03aa874a.chunk.css"
+  },
+  {
+    "revision": "227071d0affd2a5c6d83",
+    "url": "/burger-builder-deployment/static/css/3.3d37f730.chunk.css"
+  },
+  {
+    "revision": "9666c166cc8a59b536bbb0ee9bcafc2f",
+    "url": "/burger-builder-deployment/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/burger-builder-deployment/precache-manifest.80d26bf64e12da78ddcc211b30059251.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/burger-builder-deployment/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
