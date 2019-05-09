ee847eacc5b94ffdab9076aeaf635e0a88e813d1
Tran Vinh Thuy <thuy.tranvinh@vn.bosch.com>
2019-05-09 15:08:00 +0700

    Updates

---
+++ precache-manifest.239c78f0033863cfcf429747dfb96545.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "7997f2f9985fcfe1a800",
+    "url": "/reac-burger-builder/static/css/4.b4c28651.chunk.css"
+  },
+  {
+    "revision": "76b9c3303a231edb9e5f",
+    "url": "/reac-burger-builder/static/css/main.3ce1a1e0.chunk.css"
+  },
+  {
+    "revision": "c9a0c32e152034331277",
+    "url": "/reac-burger-builder/static/js/runtime~main.4fe32d73.js"
+  },
+  {
+    "revision": "ab3fa98727f51f83be70",
+    "url": "/reac-burger-builder/static/js/2.a6a8d72a.chunk.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/reac-burger-builder/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "227071d0affd2a5c6d83",
+    "url": "/reac-burger-builder/static/js/3.a2ffd715.chunk.js"
+  },
+  {
+    "revision": "76b9c3303a231edb9e5f",
+    "url": "/reac-burger-builder/static/js/main.befe67d7.chunk.js"
+  },
+  {
+    "revision": "7997f2f9985fcfe1a800",
+    "url": "/reac-burger-builder/static/js/4.2779a391.chunk.js"
+  },
+  {
+    "revision": "bb78775113cfd63c712d",
+    "url": "/reac-burger-builder/static/js/5.5c03252c.chunk.js"
+  },
+  {
+    "revision": "bb78775113cfd63c712d",
+    "url": "/reac-burger-builder/static/css/5.03aa874a.chunk.css"
+  },
+  {
+    "revision": "227071d0affd2a5c6d83",
+    "url": "/reac-burger-builder/static/css/3.3d37f730.chunk.css"
+  },
+  {
+    "revision": "4ec34e6aad1683fc9ed22b38f1cc958a",
+    "url": "/reac-burger-builder/index.html"
+  }
+];
