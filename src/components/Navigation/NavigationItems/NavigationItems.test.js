c826259a3970b28483dc326299d8646f50e13ac4
Tran Vinh Thuy <thuy.tranvinh@vn.bosch.com>
2019-05-09 15:00:40 +0700

    Revert "Updates"
    This reverts commit 752954ddd2957fdcbf3aa3b0104d948b041626cf.

---
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import NavigationItems from '../NavigationItems/NavigationItems';
+import NavigationItem from '../NavigationItems/NavigationItem/NavigationItem';
+configure({ adapter: new Adapter() });
+
+describe('<NavigationItems />', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<NavigationItems />);
+  });
+
+  it('should render 2 <NavigationItem /> elements if not authenticated', () => {
+    expect(wrapper.find(NavigationItem)).toHaveLength(2);
+  });
+
+  it('should render 3 <NavigationItem /> elements if authenticated', () => {
+    // wrapper = shallow(<NavigationItems isAuthenticated />);
+    wrapper.setProps({ isAuthenticated: true });
+    expect(wrapper.find(NavigationItem)).toHaveLength(3);
+  });
+
+  it('should render Logout <NavigationItem /> element if authenticated', () => {
+    // wrapper = shallow(<NavigationItems isAuthenticated />);
+    wrapper.setProps({ isAuthenticated: true });
+    expect(
+      wrapper.contains(<NavigationItem link="/logout">Logout</NavigationItem>)
+    ).toEqual(true);
+  });
+});
---
+++ src/components/UI/Input/Input.jsx
@@ -0,0 +1,80 @@
+import React from 'react';
+import classes from './Input.css';
+const input = props => {
+  let inputElement = null;
+  const inputClasses = [classes.InputElement];
+
+  if (props.invalid && props.shouldValidate && props.touched) {
+    inputClasses.push(classes.Invalid);
+  }
+
+  let validationError = null;
+  if (props.invalid && props.touched) {
+    validationError = props.errorMessages.map((err, idx) => (
+      <span
+        className={classes.ValidationError}
+        key={'error' + idx + props.elementConfig.placeholder.replace(' ', '')}
+      >
+        {err}
+      </span>
+    ));
+  }
+
+  switch (props.elementType) {
+    case 'text':
+      inputElement = (
+        <input
+          className={inputClasses.join(' ')}
+          {...props.elementConfig}
+          value={props.value}
+          onChange={props.changed}
+        />
+      );
+      break;
+    case 'textarea':
+      inputElement = (
+        <textarea
+          className={inputClasses.join(' ')}
+          {...props.elementConfig}
+          value={props.value}
+          onChange={props.changed}
+        />
+      );
+      break;
+    case 'select':
+      inputElement = (
+        <select
+          className={inputClasses.join(' ')}
+          value={props.value}
+          onChange={props.changed}
+        >
+          {props.elementConfig.options.map((opt, idx) => {
+            return (
+              <option value={opt.value} key={idx}>
+                {opt.displayValue}
+              </option>
+            );
+          })}
+        </select>
+      );
+      break;
+    default:
+      inputElement = (
+        <input
+          className={inputClasses.join(' ')}
+          {...props.elementConfig}
+          value={props.value}
+          onChange={props.changed}
+        />
+      );
+      break;
+  }
+  return (
+    <div className={classes.Input}>
+      <label className={classes.Label}>{props.elementConfig.placeholder}</label>
+      {inputElement}
+      {validationError}
+    </div>
+  );
+};
+export default input;
